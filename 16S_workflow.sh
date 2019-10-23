28bb0411a5781e94c9be1aed1026650d4c099a9c
lilianyc <lilianyangcrosson@gmail.com>
2019-10-23 14:36:08 +0200

    Add AlienTrimmer single-end test

--- 16S_workflow.sh
+++ 16S_workflow.sh
@@ -3,16 +3,36 @@
 # The script assumes files are arranged according to the practical directory.
 # It takes 2 positional arguments.
 
+# Positional arguments
 raw_reads_dir=$1
 output_dir=$2
+
+# Global variables.
+
 
 echo "raw reads directory: $raw_reads_dir"
 echo "output directory: $output_dir"
 
+# Creating the output directory.
 mkdir $output_dir
 
+# Creating the AlienTrimmer binary.
+./soft/JarMaker.sh soft/AlienTrimmer.java
+
+# Unzip the gz files to get fastq files.
+echo "Unzipping fastq.gz files"
+gunzip $raw_reads_dir/*.fastq.gz
 
 # Here it supposes the dir name has no "/" appended.
-for file in $raw_reads_dir/*.fastq.gz; do
-    fastqc $file -o $output_dir
+for file in $raw_reads_dir/*.fastq; do
+    #fastqc $file -o $output_dir
+    java -jar soft/AlienTrimmer.jar -i $file -q 20 -c databases/contaminants.fasta
 done
+
+#for file in $(ls $raw_reads_dir); do
+#    name_R1="$file"
+#    name_R2= 'echo $file|sed "s:R1:R2:g"'
+#    echo tes $name_R1 $name_R2
+#done
+
+
