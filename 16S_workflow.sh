53f64cc4be3438053b747e0ed4be695b5235575f
lilianyc <lilianyangcrosson@gmail.com>
2019-10-23 13:53:48 +0200

    Add fastqc output redirection

--- 16S_workflow.sh
+++ 16S_workflow.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
+# The script assumes files are arranged according to the practical directory.
+# It takes 2 positional arguments.
+
 raw_reads_dir=$1
 output_dir=$2
 
-echo $raw_reads_dir
-echo $output_dir
+echo "raw reads directory: $raw_reads_dir"
+echo "output directory: $output_dir"
+
+mkdir $output_dir
+
+
+# Here it supposes the dir name has no "/" appended.
+for file in $raw_reads_dir/*.fastq.gz; do
+    fastqc $file -o $output_dir
+done
